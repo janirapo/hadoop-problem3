1c57b59fae3521a170f0f2096ef1a9022c617202
Jani Rapo <rapo.jani@gmail.com>
2018-10-09 17:49:13 +0300

    Added initial files under trigram folder

---
+++ trigram/run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+javac JaccardMap.java -cp $(hadoop classpath) -Xlint:deprecation -Xlint:unchecked
+jar cf jm.jar JaccardMap*.class
+#hadoop jar jm.jar JaccardMap /input/s3.txt /input/s4.txt /output
+hadoop jar jm.jar JaccardMap /input/s1.txt /input/s2.txt /output
+#hadoop fs -cat /output/part-r-00000 | sort
